90b70abbecbab79fb90568d776c71f54eca06f4b
Mansi dass <116175281+Mansi-dass@users.noreply.github.com>
2023-07-14 13:24:15 +0530

    Add: Basic outline of message sending box

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -18,7 +18,7 @@ function Chat() {
                         <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z" fill='#d1d5db'></path>
                     </svg>
 
-                    <div className=' mr-4 cursor-pointer'>
+                    <div className='mr-4 cursor-pointer'>
                         <div className='w-1 h-1 m-[3px] rounded-full bg-gray-300'></div>
                         <div className='w-1 h-1 m-[3px] rounded-full bg-gray-300'></div>
                         <div className='w-1 h-1 m-[3px] rounded-full bg-gray-300'></div>
@@ -26,9 +26,13 @@ function Chat() {
                 </div>
             </nav>
             
-            <section>
+            <section className='h-[calc(100%-3.2rem)]'>
                 {/* message body */}
             </section>
+
+            <div className='relative -top-11 bg-gray-700 border-l-2 border-r-4 border-gray-600 rounded-xl  px-3 py-2'>
+                Basic Outline
+            </div>
         </div>
     )
 }
